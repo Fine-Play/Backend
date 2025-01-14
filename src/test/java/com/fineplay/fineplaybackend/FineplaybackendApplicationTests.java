b615a5b18b2d55029e6a3321f5a97153ced85f67
seoyeon2001 <syeon1250@naver.com>
2025-01-14 13:20:16 +0900

    Initial setting

---
+++ src/test/java/com/fineplay/fineplaybackend/FineplaybackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fineplay.fineplaybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FineplaybackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
