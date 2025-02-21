6ec8e89df6039540634b46b7eb2a59ee07b02589
seoyeon2001 <syeon1250@naver.com>
2025-02-21 12:44:22 +0900

    FIX: WebSecurityConfig.java

--- src/main/java/com/fineplay/fineplaybackend/config/WebSecurityConfig.java
+++ src/main/java/com/fineplay/fineplaybackend/config/WebSecurityConfig.java
@@ -1,4 +1,3 @@
-// 일단.. 이게 안됨
 package com.fineplay.fineplaybackend.config;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -9,46 +8,48 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-// 수정... 어렵다 ㅠ
-//@Configurable
 @Configuration
 //@EnableWebFluxSecurity
 @RequiredArgsConstructor
 public class WebSecurityConfig {
     private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    private final FailedAuthenticationEntryPoint failedAuthenticationEntryPoint;
 
     @Bean
-    protected SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
-//                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
-                .cors(withDefaults()) // CORS 설정
-                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
-                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // CORS 설정
+//                .csrf(csrf -> csrf.disable()) // CSRF 비활성화
+                .csrf(CsrfConfigurer::disable)
+//                .httpBasic(httpBasic -> httpBasic.disable()) // HTTP Basic 인증 비활성화
+                .httpBasic(HttpBasicConfigurer::disable)
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 정책
-                .authorizeHttpRequests(auth -> auth
+                .authorizeHttpRequests(request -> request
                         .requestMatchers("/", "/api/auth/**").permitAll() // 특정 요청 허용
-                        .requestMatchers(HttpMethod.GET, "/api/**", "/api/user/*").permitAll() // GET 요청 허용
+                        .requestMatchers(HttpMethod.GET, "/api/team/nameDuplicate").permitAll() // GET 요청 허용
                         .anyRequest().authenticated() // 그 외 인증 필요
                 )
                 .exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(new FailedAuthenticationEntryPoint()) // 인증 실패 핸들링
-                );
-        httpSecurity.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+                        .authenticationEntryPoint(failedAuthenticationEntryPoint) // 인증 실패 핸들링
+                )
+                .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
         return httpSecurity.build();
     }
@@ -56,7 +57,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.addAllowedOriginPattern("*"); // 모든 도메인 허용
+        configuration.addAllowedOrigin("*"); // 모든 도메인 허용
         configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
         configuration.addAllowedHeader("*"); // 모든 헤더 허용
         configuration.setAllowCredentials(true); // 인증 정보 포함 허용
@@ -68,6 +69,7 @@ public class WebSecurityConfig {
 }
 
 // 인증 실패 처리
+@Component
 class FailedAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     @Override
